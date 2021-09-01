94c416e420d5f7f5ca9cd9985b9d1f7433272303
dneumaier <89528548+dneumaier@users.noreply.github.com>
2021-09-01 08:04:29 -0500

    Homework1

---
+++ Assignments/HomeworkNeumaier.R
@@ -0,0 +1,9 @@
+setwd("~/Documents/GitHub/ADJHomework/Data")
+dispatch <- read.csv("dispatch.csv")
+View(dispatch)
+#What is the busiest crime time? (or when is crime time?)
+#Which day of the week has the most crime on average?
+#What is the most common reason for a call?
+#Which policing area has the highest crime rate?
+#How may officers are in each policing area?
+#What are the response times for officers to reach after the call?
