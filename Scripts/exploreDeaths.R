82f777dbba0cf18ccfb59655657fba3b6aa49a25
dneumaier <89528548+dneumaier@users.noreply.github.com>
2021-09-27 09:43:26 -0500

    Update exploreDeaths for 9/27

--- Scripts/exploreDeaths.R
+++ Scripts/exploreDeaths.R
@@ -35,5 +35,68 @@ deaths %>% filter(population != "Not Applicable") %>% group_by(year) %>%
   arrange(desc(deathRate)) %>%
   select(year, deathRate)
 
-write.csv(notes, "Data/deaths-notes.csv", na="")
-write.csv(deaths, "Data/clean1Deaths2010-2019StateYearAgeRaceEth.csv", na="")
+write_csv(notes, "Data/deaths-notes.csv", na="")
+write_csv(deaths, "Data/clean1Deaths2010-2019StateYearAgeRaceEth.csv", na="")
+
+#importing deaths to be the clean data
+deaths <- read_csv("Data/clean1Deaths2010-2019StateYearAgeRaceEth.csv")
+
+#Introducing new_pop column (numeric population)
+deaths <- deaths %>% mutate(new_pop = as.numeric(population))
+
+#creating an object that has death_by_year using the new_pop
+death_by_year <- deaths %>% filter(!is.na(new_pop)) %>%
+  group_by(year) %>% summarise(death_rate = sum(deaths)/sum(new_pop)*100000)
+
+write_csv(deaths, "Data/clean1Deaths2010-2019StateYearAgeRaceEth.csv", na="")
+
+
+# intro to ggplot2: plot death_rate by year
+ggplot(data=death_by_year, aes(x=year, y=death_rate, group=1)) +
+  geom_line()+
+  geom_point()
+
+# plot death_rate by year for each age group: 
+
+age_by_year <- deaths %>% filter(!is.na(new_pop)) %>%
+  group_by(year, ten_year_age_groups) %>% summarise(death_rate = sum(deaths)/sum(new_pop)*100000)
+
+ggplot(data=age_by_year, aes(x=year, y=death_rate, group=ten_year_age_groups, color=ten_year_age_groups)) +
+  geom_line()+
+  geom_point()
+#& for "and" | for "or", %in% is a faster solution to an "or" statement compare 2010, 2019 death rates by state
+state_year <- deaths %>% filter(!is.na(new_pop) & year %in% c("2010", "2019")) %>%
+  group_by(state, year) %>% summarize(death_rate = sum(deaths)/sum(new_pop)*100000)
+
+#essentially rotating the table to be wider than it is tall, adding percent change
+state_year %>% pivot_wider(names_from = year, values_from = death_rate) %>%
+  mutate(pct_chg = (`2019`-`2010`)/`2010`*100) %>%
+  arrange(pct_chg)
+
+#creating ageYear to compare 2010, 2019 death rates by age group
+ageYear <- deaths %>% filter(!is.na(new_pop) & year %in% c("2010", "2019")) %>%
+  group_by(ten_year_age_groups, year) %>% summarize(death_rate = sum(deaths)/sum(new_pop)*100000)
+
+#adding in percent change
+ageYear %>% pivot_wider(names_from = year, values_from = death_rate) %>%
+  mutate(pct_chg = (`2019`-`2010`)/`2010`*100) %>%
+  arrange(desc(pct_chg))
+
+#changes in death rates over all years by age groups
+ageYearAll <- deaths %>% filter(!is.na(new_pop)) %>%
+  group_by(ten_year_age_groups, year) %>% summarize(death_rate = sum(deaths)/sum(new_pop)*100000)
+
+#pivoting it to be wider
+ageYearAllPivot <- ageYearAll %>% pivot_wider(names_from = year, values_from = death_rate) %>%
+  mutate(pct_chg = (`2019`-`2010`)/`2010`*100) %>%
+  arrange(desc(pct_chg))
+
+
+#creating a line chart for highest increasing rates
+
+ageYearPair <- deaths %>% filter(!is.na(new_pop) & ten_year_age_groups %in% c("25-34 years", "35-44 years")) %>%
+  group_by(ten_year_age_groups, year) %>% summarize(death_rate = sum(deaths)/sum(new_pop)*100000)
+
+ggplot(data=ageYearPair, aes(x=year, y=death_rate, group=ten_year_age_groups, color=ten_year_age_groups)) +
+  geom_line()+
+  geom_point()
